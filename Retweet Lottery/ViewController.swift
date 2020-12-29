25a6cece18afa7377dcb87488b8ba7de84c5720e
Supannee Mutitanon <billbillgm@gmail.com>
2020-12-29 19:35:13 +0700

    init proj

---
+++ Retweet Lottery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Retweet Lottery
+//
+//  Created by Supannee Mutitanon on 29/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
